b2e515d460ba0ee8e5f09fde44f354ee4b5900a3
Maxwell Bates <max.bates@autodesk.com>
2015-10-16 16:00:47 +0100

    add example static route AboutPage

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,12 @@ import { Route } from 'react-router';
 import App from './containers/App';
 import UserPage from './containers/UserPage';
 import RepoPage from './containers/RepoPage';
+import AboutPage from './components/AboutPage';
 
 export default (
   <Route path="/" component={App}>
+    <Route path="/about"
+           component={AboutPage} />
     <Route path="/:login/:name"
            component={RepoPage} />
     <Route path="/:login"
